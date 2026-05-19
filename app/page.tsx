7b1e4e0befd1f3ba487885d73c2b5b3eb9cc4b5a
Pavel <pavk6613@gmail.com>
2026-05-19 10:00:12 +0300

    added a simple form

--- app/page.tsx
+++ app/page.tsx
@@ -1,64 +1,28 @@
-import Image from "next/image";
+'use client'
+
+function sendInformation(formData: FormData) {
+  console.log(formData.get('phoneNumber'))
+}
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
+    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans">
+      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-center py-32 px-16">
+        <form action={sendInformation} className="flex flex-col bg-black p-4 gap-4 rounded-md w-4/5">
+          <div className="flex flex-col">
+            <label htmlFor="phone-number" className="text-white">Phone number</label>
+            <input type="text" id="phone-number" name="phoneNumber" className="bg-white text-black rounded-xs h-7"/>
+          </div>
+          <div className="flex flex-col">
+            <label htmlFor="link" className="text-white">Link to job posting</label>
+            <input type="text" id="link" name="link" className="bg-white text-black rounded-xs h-7"/>
+          </div>
+          <div className="flex flex-col">
+            <label htmlFor="questions" className="text-white">Questions</label>
+            <textarea id="questions" name="questions" rows={4} className="bg-white text-black rounded-xs"></textarea>
+          </div>
+          <button type="submit" className="w-fit self-end px-5 py-2 bg-white text-black rounded-sm">Submit</button>
+        </form>
       </main>
     </div>
   );

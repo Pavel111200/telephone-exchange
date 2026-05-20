e38215fb7ffd63d1f521902364353d2ed9dabd99
Elin Burnazov <el.burnazov@gmail.com>
2026-05-20 11:46:12 +0300

    Output answers test

--- app/page.tsx
+++ app/page.tsx
@@ -1,46 +1,72 @@
 "use client";
 
 import { useEffect, useState } from "react";
-
-const [ws, setWs] = useState<WebSocket>();
-
-useEffect(() => {
-  const websocket = new WebSocket("ws://localhost:8080");
-  setWs(websocket);
-
-  websocket.onopen = () => {
-    console.log("Connection established");
-  };
-
-  websocket.onmessage = (event) => {
-    console.log(event.data);
-  };
-}, []);
-
-function sendInformation(formData: FormData) {
-  if (ws?.readyState === WebSocket.OPEN) {
-    ws.send(
-      JSON.stringify({
-        phone: formData.get("phone"),
-        link: formData.get("link"),
-        questions: formData.get("questions"),
-      }),
-    );
-  }
-}
+import Output from "./output";
 
 export default function Home() {
+  const [ws, setWs] = useState<WebSocket>();
+  const [answer, setAnswer] = useState("");
+
+  useEffect(() => {
+    // WEBSOCKET CONNECTION
+
+    /*
+    const websocket = new WebSocket("ws://localhost:8080");
+    setWs(websocket);
+
+    websocket.onopen = () => {
+      console.log("Connection established");
+    };
+
+    websocket.onmessage = (event) => {
+      console.log(event.data);
+      setAnswer(event.data);
+    };
+
+    return () => {
+      websocket.close();
+    };
+    */
+  }, []);
+
+  function sendInformation(formData: FormData) {
+    // Test
+
+    setAnswer(`
+      
+AI Response:
+This is a fake AI-generated answer for testing the frontend UI.
+    `);
+
+    // WEBSOCKET SEND
+    /*
+    if (ws?.readyState === WebSocket.OPEN) {
+      ws.send(
+        JSON.stringify({
+          phone: formData.get("phoneNumber"),
+          link: formData.get("link"),
+          questions: formData.get("questions"),
+        }),
+      );
+    }
+    */
+  }
+
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-center py-32 px-16">
+      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-center py-32 px-16 gap-6">
         <form
-          action={sendInformation}
+          onSubmit={(event) => {
+            event.preventDefault();
+            sendInformation(new FormData(event.currentTarget));
+          }}
           className="flex flex-col bg-black p-4 gap-4 rounded-md w-4/5"
         >
           <div className="flex flex-col">
             <label htmlFor="phone-number" className="text-white">
               Phone number
             </label>
+
             <input
               type="text"
               id="phone-number"
@@ -48,10 +74,12 @@ export default function Home() {
               className="bg-white text-black rounded-xs h-7"
             />
           </div>
+
           <div className="flex flex-col">
             <label htmlFor="link" className="text-white">
               Link to job posting
             </label>
+
             <input
               type="text"
               id="link"
@@ -59,10 +87,12 @@ export default function Home() {
               className="bg-white text-black rounded-xs h-7"
             />
           </div>
+
           <div className="flex flex-col">
             <label htmlFor="questions" className="text-white">
               Questions
             </label>
+
             <textarea
               id="questions"
               name="questions"
@@ -70,6 +100,7 @@ export default function Home() {
               className="bg-white text-black rounded-xs"
             ></textarea>
           </div>
+
           <button
             type="submit"
             className="w-fit self-end px-5 py-2 bg-white text-black rounded-sm"
@@ -77,6 +108,8 @@ export default function Home() {
             Submit
           </button>
         </form>
+
+        {answer && <Output answer={answer} />}
       </main>
     </div>
   );
